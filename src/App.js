64d705854cd3d5073c147a3df5f54d81df802617
Andrew N <aninneman@gmail.com>
2018-02-19 10:23:36 -0600

    merge

--- src/App.js
+++ src/App.js
@@ -11,8 +11,17 @@ class App extends Component {
     return (
       <div className="App">
 
-        <h1>HAPPY HACKING!</h1>
-        <Button bsStyle="primary">Bootstrap Test</Button>
+
+
+
+      <Switch>
+        <Route exact path='/' component={HomeView} />
+        <Route exact path='/profile' component={ProfileView} />
+        <Route exact path='/rankings' component={RankingsView} />
+        <Route exact path='/top-movies' component={MovieRankingView} />
+        <Route exact path='/search/:searchtext' component={SearchView} />
+        <Route render={ () => ( <Redirect to='/' />) } />
+      </Switch>
       </div>
     );
   }
---
+++ src/views/HomeView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HomeView = props => {
+
+  return (
+    <div>
+      Search Here
+    </div>
+  );
+}
+
+export default HomeView;
