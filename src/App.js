36ebeedc251917d99bc2a1ce4b742c748ad6426b
Andrew N <aninneman@gmail.com>
2018-02-19 14:10:38 -0600

    Reactstrap

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import {
 } from 'react-router-dom';
 import HomeView from './views/HomeView';
 import RankingsView from './views/ranking';
+import MovieGeekNav from './components/nav';
+import background from './assets/movieBackground-1.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={ {backgroundImage: background} }>
         <nav>
+          <MovieGeekNav />
         </nav>
 
 
 
 
         <Switch>
-          {/* <Route exact path='/' component={HomeView} /> */}
+          <Route exact path='/' component={HomeView} />
           {/* <Route exact path='/profile' component={ProfileView} /> */}
-          <Route exact path='/ranking' component={RankingsView} />
+          {/* <Route exact path='/ranking' component={RankingsView} /> */}
           {/* <Route exact path='/top-movies' component={MovieRankingView} /> */}
           {/* <Route exact path='/search/:searchtext' component={SearchView} /> */}
           <Route render={ () => ( <Redirect to='/ranking' />) } />
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,65 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
+import React, { Component } from 'react';
 import {
+  Collapse,
   Navbar,
-
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from 'reactstrap';
 import icon from '../assets/clapper-icon.png';
 
-const MovieGeekNav = props => {
+class MovieGeekNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    }
+  }
 
-  return (
-    <div>
-      Menu
-    </div>
-  );
+  render() {
+
+    return (
+      <div>
+      <Navbar color="faded" light expand="md">
+        <NavbarBrand href="/">Movie Geek</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu >
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      </div>
+    );
+  }
 }
 
 export default MovieGeekNav;
