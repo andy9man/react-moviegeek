e454d5ea1bdc46620c58866437fedb72d6f3f9c9
Andrew N <aninneman@gmail.com>
2018-02-19 13:57:08 -0600

    Reactstrap

--- src/views/ranking.js
+++ src/views/ranking.js
@@ -28,7 +28,7 @@ class RankingsView extends Component{
       <div className='rankcontainer'>
         <h3>Rankings - Top: 5</h3>
         {this.props.loadingData ?
-          <div><Progress active bsStyle="success" min={1} max={2} now={2} label='Loading...'/></div>
+          <div><Progress active animated color="success" value="100" max="100" now="100" label='Loading...'/></div>
           :
           // Map an object - use helper function to return what to render
           <div className='rankscroll'>
