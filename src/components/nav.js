2628a0db9dfe71919966bd85a18d118a45bb4009
Andrew N <aninneman@gmail.com>
2018-02-19 13:43:57 -0600

    Working version

---
+++ src/components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+import {
+  Navbar,
+
+} from 'reactstrap';
+import icon from '../assets/clapper-icon.png';
+
+const MovieGeekNav = props => {
+
+  return (
+    <div>
+      Menu
+    </div>
+  );
+}
+
+export default MovieGeekNav;
