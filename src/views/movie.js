e0fc88c3ffe2c903346b096721cde68b2fbc3847
golbonHagan <35237011+golbonHagan@users.noreply.github.com>
2018-02-20 13:20:08 -0600

    movie
    movie

--- src/views/movie.js
+++ src/views/movie.js
@@ -4,21 +4,34 @@ import { connect } from 'react-redux'
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText, RaisedButton, FontIcon } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import CircularProgress from 'material-ui/CircularProgress';
+import {GridList, GridTile} from 'material-ui/GridList';
+
 
 const styles = {
-  moviecontainer: {
-    // width: '600px',
-    border: '1px solid black',
-  },
-  movieposter: {
-    // width: '300px',
-  },
-  moviebox: {
+  root: {
     display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: 'lightgrey',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
   },
-}
+  gridList: {
+    // width: '100',
+    // height: '800',
+  },
+    
+    moviecontainer: {
+      // width: '600px',
+      // border: '1px solid black',
+    },
+    movieposter: {
+      // width: '300px',
+    },
+    moviebox: {
+      // display: 'flex',
+      // flexDirection: 'row',
+      // backgroundColor: 'lightgrey',
+  },
+};
+
 
 class MovieView extends Component{
   
@@ -27,31 +40,42 @@ class MovieView extends Component{
     console.log(movieObject)
 
     return (
+      <div style={styles.root}>
+    
+    <GridTile>
       <div key={idx} >
-        <Card>
-          <div style={styles.moviebox}>
-            <div style={{ width: '70%' }}>
+        <Card >
+          {/* <div >
+            <div > */}
               <CardHeader
+              
                 title={movieObject.Title}
                 subtitle={`${movieObject.Year}, ${movieObject.Rated}, ${movieObject.Runtime}`}
-                avatar={movieObject.Poster}
-              >
-              </CardHeader>
+                //avatar={movieObject.Poster}
+               
+              />
+             
+             <div style={{textAlign:'center'}}>
+                <img style={{height:200}} src={movieObject.Poster} alt=""/>
+              </div>
+          
+            
+              
               <CardTitle title={`${movieObject.Ratings[0].Source} ${movieObject.Ratings[0].Value}`} subtitle={movieObject.Plot} />
               {/* <CardText expandable={false}></CardText> */}
+              
               <CardActions>
-                <FlatButton label="Add to Favorites" primary={true} />
-                <FlatButton label="Add to Queue" secondary={true} />
+                <FlatButton label="Add to Favorites"/>
+                <FlatButton label="Add to Queue" />
               </CardActions>
-            </div>
-            <div style={{ width: '30%' }}>
-              <CardMedia>
-                <img src={movieObject.Poster} alt="" />
-              </CardMedia>
-            </div>
-          </div>
+            {/* </div>
+  
+          </div> */}
         </Card>
       </div>
+    </GridTile>
+    
+  </div>
     )
   }
 
@@ -59,6 +83,56 @@ class MovieView extends Component{
     let localMovie = []
     if(this.props.movieData) localMovie = this.props.movieData
     localMovie = [
+      {
+        Title: "Batman",
+        Year: "1989",
+        Rated: "PG-13",
+        Released: "23 Jun 1989",
+        Runtime: "126 min",
+        Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker. The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.",
+        Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
+        Ratings:[{Source: "Rotten Tomatoes", Value: "72%"}]
+      },
+      {
+        Title: "Batman",
+        Year: "1989",
+        Rated: "PG-13",
+        Released: "23 Jun 1989",
+        Runtime: "126 min",
+        Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker. The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.",
+        Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
+        Ratings:[{Source: "Rotten Tomatoes", Value: "72%"}]
+      },
+      {
+        Title: "Batman",
+        Year: "1989",
+        Rated: "PG-13",
+        Released: "23 Jun 1989",
+        Runtime: "126 min",
+        Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker. The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.",
+        Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
+        Ratings:[{Source: "Rotten Tomatoes", Value: "72%"}]
+      },
+      {
+        Title: "Batman",
+        Year: "1989",
+        Rated: "PG-13",
+        Released: "23 Jun 1989",
+        Runtime: "126 min",
+        Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker. The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.",
+        Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
+        Ratings:[{Source: "Rotten Tomatoes", Value: "72%"}]
+      },
+      {
+        Title: "Batman",
+        Year: "1989",
+        Rated: "PG-13",
+        Released: "23 Jun 1989",
+        Runtime: "126 min",
+        Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker. The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.",
+        Poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg",
+        Ratings:[{Source: "Rotten Tomatoes", Value: "72%"}]
+      },
       {
         Title: "Batman",
         Year: "1989",
@@ -79,8 +153,12 @@ class MovieView extends Component{
           </div>
         :
           // Map an object - use helper function to return what to render
-          <div>
+          <div style={styles.root}>
+            <GridList
+             cellHeight={700}
+             style={styles.gridList}>
             {localMovie.map(this.movieMap)}
+            </GridList>
           </div>
         }
       </div>
