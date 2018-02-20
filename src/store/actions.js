a937ff7bb927e47e0e6aef6c0e7e5777511d4656
golbonHagan <35237011+golbonHagan@users.noreply.github.com>
2018-02-20 16:31:03 -0600

    top50
    top

--- src/store/actions.js
+++ src/store/actions.js
@@ -32,7 +32,7 @@ export const getTopMovie = () => {
     dispatch(dataResultHandler(DATA_STATUS_HANDLER, 'loadingData', true));
     console.log(`Getting Data... ${url}`);
     //axios.get(`http://5a8b07983d92490012370bba.mockapi.io/notes`)
-    axios.get(`http://5a8b1dc33d92490012370bcc.mockapi.io/top50`)
+    axios.get(`http://5a8c94d2d6c8840012dde929.mockapi.io/api/v1/TopMovies`)
       .then(({ data }) => {
         //setTimeout( () => { dispatch( {type: LOAD_DATA, payload: products} ) }, 1);
         dispatch({ type: LOAD_TOPMOVIES, payload: data });
